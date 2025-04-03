12e3fa7fc524e95938f21cf8d0b9d1bd087429c9
Alex Crubaugh <abcrubaugh@uncg.edu>
2025-04-03 15:17:30 -0400

    Delete provider-backend/Group-5-master directory

--- provider-backend/Group-5-master/src/main/java/com/univents/univents_api/Reviews/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.univents.univents_api.Reviews;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReviewRepository extends JpaRepository<Review, Integer> {
-
-}
--- provider-backend/Group-5-master/src/main/java/com/univents/univents_api/Users/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.univents.univents_api.Users;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
